c74df1d3fbb1ac5f51d98209e5882801a41c589b
Thalison <thalissfernandes99@gmail.com>
2024-06-30 22:37:11 -0300

    Formatter executado

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,4 +12,3 @@ def test_root_should_return_ok_and_hello_world():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Hello World!'}  # Assert
-
