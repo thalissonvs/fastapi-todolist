ca7362be67bd36a8201f425d8f29e7240de63ed2
Thalison <thalissfernandes99@gmail.com>
2024-07-31 00:41:58 -0300

    Implementando o refresh do token e testes de autorização

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import factory
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -8,6 +9,15 @@ from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
 from fast_zero.security import create_access_token, get_password_hash
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'test{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
 
 
 @pytest.fixture()
@@ -50,6 +60,15 @@ def user(session):
 
     user.clean_password = 'testtest'  # monkey patching
 
+    return user
+
+
+@pytest.fixture()
+def other_user(session):
+    user = UserFactory()
+    session.add(user)
+    session.commit()
+    session.refresh(user)
     return user
 
 
--- tests/test_users.py
+++ tests/test_users.py
@@ -99,9 +99,9 @@ def test_update_user(client, user, token):
     }
 
 
-def test_update_user_error(client, token):
+def test_update_user_with_wrong_user(client, other_user, token):
     response = client.put(
-        '/users/-1',
+        f'/users/{other_user.id}',
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -124,9 +124,9 @@ def test_delete_user(client, user, token):
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_user_error(client, token):
+def test_delete_user_with_wrong_user(client, other_user, token):
     response = client.delete(
-        '/users/-1',
+        f'/users/{other_user.id}',
         headers={'Authorization': 'Bearer ' + token},
     )
 
